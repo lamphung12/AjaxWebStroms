a2e05a7935ac11fb8d6b3bdf5bdd12e0c1f70b8a
Your Name <phunglam11d@gamil.com>
2022-06-23 20:47:09 +0700

    ajax

---
+++ Visual/demo.js/baitap.js
@@ -0,0 +1,16 @@
+arr = [1,5,7,8,9,15,8,12];
+///In ra các số chẵn và lẻ của mảng
+for(var i=0;i<arr.length;i++){
+    if(arr[i]%2==0){
+        console.log(arr[i])
+    }
+}
+///in ra các số lướn hơn hoặc bằng 6
+/////cộng tổng các số chia hết cho 2 của mảng
+///tính tích 3 số đầu của mảng
+///tìm ra xem mảng có bao nhiêu phần tử là số 5
+// đếm số phần tử =8 trong mảng
+// tính tổng 3 phần tử cuối của mảng
+
+
+
